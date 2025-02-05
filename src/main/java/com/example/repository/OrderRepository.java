7cdbd6926fc6101e70a3f421691fe1491ff955df
Vitalii Martiniuc <vitalimartiniuc@gmail.com>
2025-02-06 00:51:53 +0100

    add query for find food

--- src/main/java/com/example/repository/OrderRepository.java
+++ src/main/java/com/example/repository/OrderRepository.java
@@ -1,17 +1,23 @@
 package com.example.repository;
 
-import java.util.List;
-
+import com.example.model.Order;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.example.model.Order;
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
 	@Query("SELECT o FROM Order o WHERE o.customer.id = :userId")
-	List<Order> findAllUserOrders(@Param("userId")Long userId);
-    
+	List<Order> findAllUserOrders(@Param("userId") Long userId);
+
 	@Query("SELECT o FROM Order o WHERE o.restaurant.id = :restaurantId")
 	List<Order> findOrdersByRestaurantId(@Param("restaurantId") Long restaurantId);
+
+	@Query("SELECT DISTINCT o FROM Order o JOIN o.items i WHERE i.food.id = :foodId AND o.orderStatus = :status")
+	List<Order> findByFoodAndStatus(@Param("foodId") Long foodId, @Param("status") String status);
+
+
 }
