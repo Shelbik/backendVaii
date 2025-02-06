9a5e22c92300ca75fe9a3cf8bcbe7f3b53e13e85
Vitalii Martiniuc <vitalimartiniuc@gmail.com>
2025-02-06 18:22:33 +0100

    add query to delete food

--- src/main/java/com/example/repository/CartItemRepository.java
+++ src/main/java/com/example/repository/CartItemRepository.java
@@ -1,12 +1,17 @@
 package com.example.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.model.CartItem;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
-
-//    CartItem findByFoodIsContaining
-
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM CartItem ci WHERE ci.food.id = :foodId")
+    void deleteByFoodId(@Param("foodId") Long foodId);
 }
